d3e62e4856020ab4d3cae9755936802077f04f8d
Benjamin Bjorkman <bpbjorkman@ucdavis.edu>
2017-11-15 23:24:01 -0800

    Built a little

--- src/main/test/core/test/TestCourseManager.java
+++ src/main/test/core/test/TestCourseManager.java
@@ -1,6 +1,7 @@
 package core.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
